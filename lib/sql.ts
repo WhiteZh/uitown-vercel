444622a6fa59874c1263c99a38570c94971db46a
johnbonjour <johnbonjoursignin@outlook.com>
2024-12-31 17:58:46 -0800

    removed useless code

--- lib/sql.ts
+++
@@ -1,9 +0,0 @@
-import {neon} from "@neondatabase/serverless";
-
-const DATABASE_URL = process.env["DATABASE_URL"];
-
-if (DATABASE_URL === undefined) {
-    throw Error("Missing environmental variable DATABASE_URL");
-}
-
-export default neon(DATABASE_URL);
