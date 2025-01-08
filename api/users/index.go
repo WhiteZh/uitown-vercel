9c2d1d3728f37ea937cb5a47f537c3f40d6d5e50
John Bonjour <whitewolfzhang@gmail.com>
2025-01-08 11:43:30 -0800

    implemented API PATCH /api/users

--- api/users/index.go
+++ api/users/index.go
@@ -4,7 +4,9 @@ import (
 	"encoding/base64"
 	"fmt"
 	"log"
+	"maps"
 	"net/http"
+	"strings"
 	"uitown-vercel/lib/utils"
 )
 
@@ -90,5 +92,138 @@ var methodRouter = utils.MethodRouter{
 
 		utils.SetContentTypeJSON(w)
 		utils.EncodeJSONOrPanic(w, res)
+	},
+	Patch: func(w http.ResponseWriter, r *http.Request) {
+		queries := r.URL.Query()
+		{
+			err := utils.UnescapeQueryValues(queries)
+			if err != nil {
+				utils.WriteBadRequestResponse(w)
+				return
+			}
+		}
+
+		params := struct {
+			id                int
+			passwordHashed    string
+			name              *string
+			email             *string
+			newPasswordHashed *string
+			description       *string
+			icon              *string
+			iconType          *string
+		}{}
+
+		{
+			_, err := fmt.Sscanf(queries.Get("id"), "%d", &params.id)
+			if err != nil {
+				utils.WriteBadRequestResponse(w)
+				return
+			}
+		}
+		{
+			params.passwordHashed = queries.Get("password_hashed")
+			if params.passwordHashed == "" {
+				utils.WriteBadRequestResponse(w)
+				return
+			}
+		}
+		{
+			name := queries.Get("name")
+			if name != "" {
+				params.name = &name
+			}
+		}
+		{
+			email := queries.Get("email")
+			if email != "" {
+				params.email = &email
+			}
+		}
+		{
+			newPasswordHashed := queries.Get("new_password_hashed")
+			if newPasswordHashed != "" {
+				params.newPasswordHashed = &newPasswordHashed
+			}
+		}
+		{
+			description := queries.Get("description")
+			if description != "" {
+				params.description = &description
+			}
+		}
+		{
+			icon := queries.Get("icon")
+			iconType := queries.Get("icon_type")
+			if icon != "" {
+				if iconType == "" {
+					utils.WriteBadRequestResponse(w)
+					return
+				}
+				params.icon = &icon
+				params.iconType = &iconType
+			}
+		}
+
+		db := utils.ConnectDBOrPanic()
+		defer utils.CloseDBOrPanic(db)
+
+		{
+			row := utils.QueryRowDBOrPanic(db, `SELECT password_hashed FROM users WHERE id = $1`, params.id)
+			var passwordHashed string
+			utils.ScanOrPanic(row, &passwordHashed)
+
+			if passwordHashed != params.passwordHashed {
+				utils.WriteUnauthorizedResponse(w)
+				return
+			}
+		}
+
+		modColumns := make(map[string]any)
+		if params.name != nil {
+			modColumns["name"] = params.name
+		}
+		if params.email != nil {
+			modColumns["email"] = params.email
+		}
+		if params.newPasswordHashed != nil {
+			modColumns["password_hashed"] = params.newPasswordHashed
+		}
+		if params.description != nil {
+			modColumns["password_hashed"] = params.description
+		}
+		if params.icon != nil {
+			if params.iconType == nil {
+				log.Panicln("miracle happened")
+			}
+			modColumns["icon"] = params.icon
+			modColumns["icon_type"] = params.iconType
+		}
+
+		var updateClause string
+		updateValues := make([]any, len(modColumns))
+		{
+			updateSegments := make([]string, len(modColumns))
+			i := 0
+			for v := range maps.Keys(modColumns) {
+				updateSegments[i] = fmt.Sprintf("%s = $%d", v, i+1)
+				updateValues[i] = modColumns[v]
+				i++
+			}
+			updateClause = strings.Join(updateSegments, ", ")
+		}
+
+		{
+			_, err := db.Exec(fmt.Sprintf("UPDATE users SET %s WHERE id = %d", updateClause, params.id), updateValues...)
+			if err != nil {
+				utils.WriteErrorResponse(
+					w,
+					"Unprocessable Entity; most likely because the new name or email conflicts with some existing one(s)",
+					http.StatusUnprocessableEntity)
+				return
+			}
+		}
+
+		w.WriteHeader(http.StatusOK)
 	},
 }
