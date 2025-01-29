2a52d6c11cf7f460b4b9b3bb5f1ec73dc0f7ad01
John Bonjour <whitewolfzhang@gmail.com>
2025-01-28 22:54:51 -0800

    small ui adjustment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,7 @@ playedOA.value = true;
   <div class="mx-auto mt-32 max-w-screen-2xl px-5 flex flex-row text-white">
     <div id="house-of-ui-img" class="lg:block hidden"></div>
     <div class="max-w-[48rem] px-10">
-      <h5 class="font-thin tracking-widest mt-12 text-sm">HI I'M UI-TOWN</h5>
+      <h5 class="font-light tracking-widest mt-12 text-sm">HI I'M UI-TOWN</h5>
       <h1 class="font-thin text-7xl text-left text-primary mt-16">A town of UI which brings your design to life.</h1>
       <h4 class="tracking-widest leading-9 text-lg max-w-lg ps-1.5 font-[Cooljazz] mt-16">"At UI-town, we offer a vast array of CSS and JS animations to elevate your web projects. Our dynamic animations and interactive effects make your user interfaces not just functional but truly captivating."</h4>
       <RouterLink :to="{name: 'browse', params: {category: ''}}" class="ms-0.5 w-40 mt-8 py-4 text-center rounded-full inline-block bg-primary text-stress hover:text-white">Enter</RouterLink>
